1fba73e445a09334e45e0ce5ec82d28a56b74b42
JdMary <mariem.jrad@esprit.tn>
2025-02-11 12:13:41 +0100

    routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./core/shared/home/home.component";
+import {ResidencesComponent} from "./residences/residences.component";
+import {NotfoundComponent} from "./core/shared/notfound/notfound.component";
+import {ResidenceDetailsComponent} from "./residence-details/residence-details.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "",redirectTo:"home",pathMatch:"full"},
+  {path: "home",component:HomeComponent},
+  {path: "residences",component:ResidencesComponent},
+  {path: "details/:id",component:ResidenceDetailsComponent},
+  {path: "**",component:NotfoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { TestComponent } from './test/test.component';
 import { MariemjradComponent } from './mariemjrad/mariemjrad.component';
 import { ResidencesComponent } from './residences/residences.component';
 import {FormsModule} from "@angular/forms";
+import { HeaderComponent } from './core/shared/header/header.component';
+import { FooterComponent } from './core/shared/footer/footer.component';
+import { HomeComponent } from './core/shared/home/home.component';
+import { NotfoundComponent } from './core/shared/notfound/notfound.component';
+import { ResidenceDetailsComponent } from './residence-details/residence-details.component';
 //au dessus ce sont les appels des classesou se trouve les imports (dir node modules)
 
 @NgModule({
@@ -14,7 +19,12 @@ import {FormsModule} from "@angular/forms";
     AppComponent,
     TestComponent,
     MariemjradComponent,
-    ResidencesComponent
+    ResidencesComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    NotfoundComponent,
+    ResidenceDetailsComponent
   ],
   //liste de packages used in this module by component d'import explicite
   imports: [
