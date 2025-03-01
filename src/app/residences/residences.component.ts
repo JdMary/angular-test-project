faa2792a98c74565d640a2a0dc71dab7050656f4
JdMary <mariem.jrad@esprit.tn>
2025-03-01 12:00:00 +0100

    service creation + Dependency Injection

--- src/app/residences/residences.component.ts
+++ src/app/residences/residences.component.ts
@@ -16,14 +16,6 @@ export class ResidencesComponent {
   id!: number;
   favorites:Residence[]=[];
   //ou bien address!:string;
-  listResidences:Residence[]=[
-    {id:1,"name": "El fel","address":"Borj Cedria","image":"../../assets/images/R1.jpeg", status: "Disponible"},
-    {id:2,"name": "El yasmine","address":"Ezzahra","image":"../../assets/images/R2.jpg", status:"Disponible" },
-    {id:3,"name": "El Arij","address":"Rades","image":"../../assets/images/R3.jpg", status: "Vendu"},
-    {id:4,"name": "El Anber","address":"inconnu","image":"../../assets/images/R4.jpg", status: "En Construction"},
-    {id:4,"name": "El Anber","address":"inconnu","image":"../../assets/images/R4.jpg", status: "En Construction"}
-  ];
-
 
   showLocation(r: Residence) {
     if (r.address === "inconnu") {
@@ -52,6 +44,20 @@ export class ResidencesComponent {
   getLocation(r: Residence) {
     this.showAdress=true;
     this.id=r.id;
+
+  }
+
+  listResidences:Residence[]=[
+    {id:1,"name": "El fel","address":"Borj Cedria","image":"../../assets/images/R1.jpeg", status: "Disponible"},
+    {id:2,"name": "El yasmine","address":"Ezzahra","image":"../../assets/images/R2.jpg", status:"Disponible" },
+    {id:3,"name": "El Arij","address":"Rades","image":"../../assets/images/R3.jpg", status: "Vendu"},
+    {id:4,"name": "El Anber","address":"inconnu","image":"../../assets/images/R4.jpg", status: "En Construction"},
+    {id:4,"name": "El Anber","address":"inconnu","image":"../../assets/images/R4.jpg", status: "En Construction"}
+  ];
+
+  constructor(Activated) {
+  }
+  ngOnInit():void{
 
   }
 }
